bf83682afde2b31a839f1cff8ea79a7367bdc228
phirook223 <rascalcamel@naver.com>
2015-03-21 21:36:03 +0900

    modify ewha_ssp_hw1_1.cpp in HW

--- ewha_ssp_hw1_1.cpp
+++ ewha_ssp_hw1_1.cpp
@@ -5,20 +5,29 @@
  * GitHub Setup Homework 
  * Instructor: Prof. HyungJune Lee
  *
- * Coded by Firstname Lastname
- * Mar xx, 2015
+ * Coded by Lee EunYoung
+ * Mar 21, 2015
  *
  */
 
 // Add #define pre-processor below
+#include "LED_test.h"
 
+#define FRONT_LED_PIN 10
+#define REAR_LED_PIN 9
  
 void setup() {
   //Add your code of Lab Assignment #1
-
+	pinMode(FRONT_LED_PIN, OUTPUT);
+	pinMode(REAR_LED_PIN, OUTPUT);
 }
 
 void loop() {
-  //Add your code of Lab Assignment #1  
-
+  //Add your code of Lab Assignment #1  	
+	digitalWrite(FRONT_LED_PIN, HIGH);
+	digitalWrite(REAR_LED_PIN, LOW);
+	delay(1000);
+	digitalWrite(FRONT_LED_PIN, LOW);
+	digitalWrite(REAR_LED_PIN, HIGH);
+	delay(1000);
 }
